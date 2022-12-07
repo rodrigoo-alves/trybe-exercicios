bed37d9cacce22b5f8ecb34c0e661c11160f4e9c
Rodrigo Alves <alvesrodrigo762@gmail.com>
2022-12-07 19:24:54 -0300

    Arrumando erros

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-3.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-3.js
@@ -1,18 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let count = 0;
-let average = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-  count += numbers[index] ;
-    
+  count += numbers[index] ;  
 }
 
-average = count / numbers.length;
-if (average > 20) {
-  console.log('valor maior que 20');
-} else {
-  console.log('valor menor ou igual a 20');
-}
-
-
+let average = count / numbers.length;
 console.log(average);
