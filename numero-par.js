2b6ccfecaf222fde6f7e39ec96e86cd02c4e8c85
Rodrigo Alves <alvesrodrigo762@gmail.com>
2022-12-07 14:40:10 -0300

    checando se são numeros impares

--- numero-par.js
+++ numero-par.js
@@ -2,6 +2,8 @@ const num1 = 1;
 const num2 = 3;
 const num3 = 5;
 
+let isEven = false;
+
 if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
-    console.log("Um desses numeros são par");
+    isEven = true;
 }
