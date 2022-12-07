a8ff4f6b7be1c628161996313bdcd1f467344521
Rodrigo Alves <alvesrodrigo762@gmail.com>
2022-12-07 18:42:20 -0300

    realizando a media maior ou menor que 20

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-4.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-4.js
@@ -3,8 +3,7 @@ let count = 0;
 let average = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-  count += numbers[index] ;
-    
+  count += numbers[index] ;  
 }
 
 average = count / numbers.length;
@@ -13,6 +12,3 @@ if (average > 20) {
 } else {
   console.log('valor menor ou igual a 20');
 }
-
-
-console.log(average);
