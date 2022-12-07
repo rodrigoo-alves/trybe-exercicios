e563ae4958d37ff382ade73148cc947164ffe001
Rodrigo Alves <alvesrodrigo762@gmail.com>
2022-12-07 19:15:34 -0300

    refatorando exercicio

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-6.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-6.js
@@ -1,10 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let oddNumbers = [];
+let oddNumbers = 0;
 
 for  (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] % 2 === 1) {
-    oddNumbers.push(numbers[index]);
+  if (numbers[index] % 2 !== 0) {
+    oddNumbers += 1;
   }
 }
 
-console.log(oddNumbers);
+if (oddNumbers === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(oddNumbers);
+}
